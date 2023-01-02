9996d41d641cda004b11f83b9dd837bbb5763cbf
Pawan Kumar <pawkumar16@publicisgroupe.net>
2023-01-02 12:03:16 +0530

    file added

---
+++ src/main/java/com/sapient/app/App.java
@@ -0,0 +1,16 @@
+package com.sapient.app;
+
+import com.sapient.model.Message;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Message message= new Message("hello world");
+        System.out.println(message.getGreeting());
+    }
+}
